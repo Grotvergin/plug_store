6a8848f935a996034b48a8574e43bdcacb4db643
Roma <mishenin_r@mail.ry>
2024-03-10 21:07:28 +0300

    waiting data from the consumer

--- main.py
+++ main.py
@@ -10,5 +10,44 @@ def Main() -> None:
             Stamp(traceback.format_exc(), 'e')
 
 
+def SendMessage(user: int, msg: Union[str, list[str]]) -> None:
+    Stamp(f'Sending message to one user {user}', 'i')
+    if isinstance(msg, str):
+        bot.send_message(user, msg, parse_mode='Markdown')
+    elif msg:
+        for m in msg:
+            bot.send_message(user, m, parse_mode='Markdown')
+    else:
+        bot.send_message(user, '▪️Нет изменений', parse_mode='Markdown')
+
+
+def ShowCatalog(message: telebot.types.Message) -> None:
+    Stamp(f'User {message.from_user.id} requested {message.text}', 'i')
+    markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    for category in PRODUCTS.keys():
+        markup.row(telebot.types.KeyboardButton(category))
+    bot.send_message(message.from_user.id, 'Выберите категорию:', reply_markup=markup)
+
+
+def ShowCategory(message: telebot.types.Message) -> None:
+    Stamp(f'User {message.from_user.id} requested {message.text}', 'i')
+    markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    for product in PRODUCTS[message.text]:
+        markup.row(telebot.types.KeyboardButton(product))
+    bot.send_message(message.from_user.id, 'Выберите товар:', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def MessageAccept(message: telebot.types.Message) -> None:
+    Stamp(f'User {message.from_user.id} requested {message.text}', 'i')
+    if message.text == '/start':
+        SendMessage(message.from_user.id, f'Привет, {message.from_user.first_name}!')
+        ShowCatalog(message)
+    elif message.text in PRODUCTS.keys():
+        ShowCategory(message)
+    else:
+        SendMessage(message.from_user.id, 'Неизвестная опция...')
+
+
 if __name__ == '__main__':
     Main()
--- source.py
+++ source.py
@@ -1,37 +1,9 @@
-import telebot
-from configparser import ConfigParser
-from pathlib import Path
-import os
-from datetime import datetime, timedelta, date
-from colorama import Fore, Style, init
-import traceback
-
-
-def Stamp(message: str, level: str) -> None:
-    time_stamp = datetime.now().strftime('[%m-%d|%H:%M:%S]')
-    match level:
-        case 'i':
-            print(Fore.LIGHTBLUE_EX + time_stamp + '[INF] ' + message + '.' + Style.RESET_ALL)
-        case 'w':
-            print(Fore.LIGHTMAGENTA_EX + time_stamp + '[WAR] ' + message + '!' + Style.RESET_ALL)
-        case 's':
-            print(Fore.LIGHTGREEN_EX + time_stamp + '[SUC] ' + message + '.' + Style.RESET_ALL)
-        case 'e':
-            print(Fore.RED + time_stamp + '[ERR] ' + message + '!!!' + Style.RESET_ALL)
-        case 'l':
-            print(Fore.WHITE + time_stamp + '[SLE] ' + message + '...' + Style.RESET_ALL)
-        case 'b':
-            print(Fore.LIGHTYELLOW_EX + time_stamp + '[BOR] ' + message + '.' + Style.RESET_ALL)
-        case _:
-            print(Fore.WHITE + time_stamp + '[UNK] ' + message + '?' + Style.RESET_ALL)
-
-
-def ParseConfig(direct: str = '') -> (ConfigParser, list):
-    config = ConfigParser()
-    config.read(Path.cwd() / direct / 'config.ini', encoding='utf-8')
-    sections = config.sections()
-    return config, sections
-
+from common import *
 
 config, _ = ParseConfig()
 bot = telebot.TeleBot(config['DEFAULT']['Token'])
+PRODUCTS = {'Пробковый пол замковый': ('Пробка хуебка', 'Вторая пробка', 'Вообще разъеб а не пробка'),
+            'Пробковый пол клеевой': ('Вторая пробка', 'Вообще разъеб а не пробка'),
+            'Настенная пробка': None,
+            'Пробковая подложка': None,
+            'Сопутствующий товар': None }
